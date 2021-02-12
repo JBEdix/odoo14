e71ff4af21c6c2cb5a9af146acdb96f339ab5c67
JBEdix <jheral.blanco@gmail.com>
2021-02-12 22:35:38 +0000

    [ADD] Internalizacion

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields, api, exceptions
+from odoo import models, fields, api, exceptions, _
 
 class Course(models.Model): #Nombre de la clase, en la base de datos
     _name = 'openacademy.course' #nombre de tabla openacademy_course
@@ -16,11 +16,11 @@ class Course(models.Model): #Nombre de la clase, en la base de datos
         default = dict(default or {})
 
         copied_count = self.search_count(
-            [('name', '=like', u"Copy of {}%".format(self.name))])
+            [('name', '=like',  _(u"Copy of {}%").format(self.name))])
         if not copied_count:
-            new_name = u"Copy of {}".format(self.name)
+            new_name = _(u"Copy of {}").format(self.name)
         else:
-            new_name = u"Copy of {} ({})".format(self.name, copied_count)
+            new_name = _(u"Copy of {} ({})").format(self.name, copied_count)
 
         default['name'] = new_name
         return super(Course, self).copy(default)
